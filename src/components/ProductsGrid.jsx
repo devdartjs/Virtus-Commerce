0ddd33fb15a1af46b654a61d6a23a500fe7ee561
Dartagnan <solucoesconsultoria542@gmail.com>
2025-07-26 09:47:55 -0300

    correcting child error on ProductsGris component

--- src/components/ProductsGrid.jsx
+++ src/components/ProductsGrid.jsx
@@ -1,11 +1,13 @@
 import { Product } from "./Product";
 
 export function ProductsGrid({ products, loadCart }) {
+  console.log(products);
   return (
     <main className="max-w-7xl mx-auto py-8 px-4">
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6">
         {products.map((product) => (
           <Product
+            key={product.id}
             product={product}
             productId={product.id}
             loadCart={loadCart}
